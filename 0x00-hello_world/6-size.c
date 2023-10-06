93dec692757249400032014736e20f8ff1c6a00e
Kelvin <kelvinnyagaka7@gmail.com>
2023-10-05 19:54:13 -0700

    helm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 [200~#include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ * 
+ * Return: Always 0 (success)
+ */
 int main (void)
 {
 	printf("size of char: %lu byte(s)\n", sizeof(char));
