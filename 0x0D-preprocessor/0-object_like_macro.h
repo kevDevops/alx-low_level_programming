7a22908056f98313816dd4c2c4de117c089c9247
kevDevops <kelvinnyagaka7@gmail.com>
2023-11-13 16:40:01 -0800

    golo

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -19,21 +19,3 @@
 
 
 #endif
-
-
-===2-main.c===
-
-
-#include <stdio.h>
-/**
-*main - prints the name of the file
-*
-*Return: always 0
-*/
-
-
-int main(void)
-{
-   printf("%s\n", __FILE__);
-   return (0);
-}
